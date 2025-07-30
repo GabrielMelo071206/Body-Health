38f7ef56a25803c93f3321e7d0e57dde99e29dbb
Gabriel Melo <gabrag0987a@gmail.com>
2025-07-30 18:09:14 -0300

    aplicação Web

--- main.py
+++ main.py
@@ -14,6 +14,36 @@ async def get_root():
    response = templates.TemplateResponse("index.html", {"request": {}})
    return response
 
+@app.get("/suporte")
+async def get_root():
+   response = templates.TemplateResponse("suporte.html", {"request": {}})
+   return response
+
+@app.get("/sobre")
+async def get_root():
+   response = templates.TemplateResponse("sobre.html", {"request": {}})
+   return response
+
+@app.get("/planos")
+async def get_root():
+   response = templates.TemplateResponse("planos.html", {"request": {}})
+   return response
+
+@app.get("/pagamento")
+async def get_root():
+   response = templates.TemplateResponse("pagamento.html", {"request": {}})
+   return response
+
+@app.get("/login")
+async def get_root():
+   response = templates.TemplateResponse("login.html", {"request": {}})
+   return response
+
+@app.get("/cadastro")
+async def get_root():
+   response = templates.TemplateResponse("cadastro.html", {"request": {}})
+   return response
+
 if __name__ == "__main__":
     uvicorn.run(app="main:app", host="127.0.0.1", port=8000, 
     reload=True)
